e107fa51cb4538b3abaa5c08f9f88dee5da5136a
buildcodewithshubham <codewithshubham.pune@gmail.com>
2025-05-27 00:12:06 +0530

    Add files via upload

---
+++ Addition4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int Addition(int A, int B)
+{
+    int Add = 0;
+    Add =  A + B;
+    return Add;
+}
+
+
+int main()
+{ 
+    int No1 = 0;
+    int No2 = 0;
+    int Ans = 0;
+
+    printf("Enter First Number : \n");
+    scanf("%d",&No1);
+
+    printf("Enter Second Number : \n");
+    scanf("%d",&No2);
+
+     Ans = Addition(No1,No2);
+
+    printf("Addition is : %d\n",Ans);
+
+    return 0;
+}
---
+++ ArrayDemo2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int Arr[4] = {10,20,30,40};
+
+   
+    printf("%lu\n",(&Arr)+1);
+
+    return 0;
+}
